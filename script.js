22cbc6f91ca00fd55a696008c84699f2ce5d4592
dynamic109 <120388304+dynamic109@users.noreply.github.com>
2024-07-05 19:49:56 +0100

    Add files via upload
    added styling for different device sizes

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 let shareIcon = document.getElementById("share-icon");
 let footer = document.getElementById("footer");
+let windowWidth = window.innerWidth;
 
 shareIcon.addEventListener("click", () => {
   footer.style.background = "hsl(217, 19%, 35%)";
-  footer.style.borderRadius = "0 0 10px 10px";
-  shareIcon.style.background = "hsl(212, 23%, 69%)";
-  shareIcon.style.color = "white";
-  shareIcon.style.width = "10px";
-  shareIcon.style.padding = "10px";
 
-  footer.innerHTML = `<div class="footer-container">
+  if (windowWidth < "1024px") {
+    footer.innerHTML = `<div class="footer-container">
                             <div class="socials-container">
                                 <p>SHARE </p>
                                 <img src="./images/icon-facebook.svg"/>
@@ -18,4 +15,15 @@ shareIcon.addEventListener("click", () => {
                             </div>
                             <img  class="share-icon" src="./images/icon-share.svg"/>
                       </div>`;
+  } else {
+    footer.innerHTML = `<div class="footer-container">
+                            <div class="socials-container">
+                                <p>SHARE </p>
+                                <img src="./images/icon-facebook.svg"/>
+                                <img src="./images/icon-twitter.svg"/>
+                                <img src="./images/icon-pinterest.svg"/>
+                            </div>
+                            <img  class="share-icon" src="./images/icon-share.svg"/>
+                      </div>`;
+  }
 });
